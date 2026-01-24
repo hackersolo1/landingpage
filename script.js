3e46b7245896a498f548b62c86cce2cc08f268dc
hackersolo1 <143674895+hackersolo1@users.noreply.github.com>
2026-01-24 14:45:24 -0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const autoRotateBtn = document.querySelector('.stopAutoRotate');
-    const autoRotateBtnI = document.querySelector('.rotateI');
+document.addEventListener("DOMContentLoaded", function() {
     const modelViewer = document.querySelector('model-viewer');
+    const stopButton = document.querySelector('.stop');
+    const rotateIcon = document.querySelector('.rotateI');
 
-    if (autoRotateBtn && modelViewer) {
-        autoRotateBtn.addEventListener('click', () => {
-            if(modelViewer.hasAttribute('auto-rotate')) {
-                modelViewer.removeAttribute('auto-rotate');
-                if (autoRotateBtnI) autoRotateBtnI.style.animationPlayState = 'paused';
-            } else {
-                modelViewer.setAttribute('auto-rotate', '');
-                if (autoRotateBtnI) autoRotateBtnI.style.animationPlayState = 'running';
-            }
-        });
-    }
+    stopButton.addEventListener('click', function() {
+        if (modelViewer.hasAttribute('auto-rotate')) {
+            modelViewer.removeAttribute('auto-rotate');
+            rotateIcon.style.animationPlayState = 'paused';
+            stopButton.title = "Iniciar a rotação automática";
+        } else {
+            modelViewer.setAttribute('auto-rotate', '');
+            rotateIcon.style.animationPlayState = 'running';
+            stopButton.title = "Parar a rotação automática";
+        }
+    });
 });
